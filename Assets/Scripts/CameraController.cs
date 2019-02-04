e42cac942978aa6d9ffd9e428ee0b39a26c49e1f
Gleb <kosia4enko.g@ex.ua>
2019-02-04 11:14:09 +0200

    vikital7 changes added

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,43 +5,36 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    public GameObject botWall;
-    public GameObject topWall;
     public GameObject fish;
     public Transform cam;
-    public float noFishZone = 0.2f;
-    //public float minimumBoundary;
-    //public float maximumBoundary;
-   
+    public float noFishZoneCoeff = 0.25f; //Частина фону, де буде діяти виштовхувальна сила
+
+    private float topBoundary;
+    private float bottomBoundary;
+
     private Vector3 offset;
     private Vector2 steerForce;
     private Rigidbody2D frb;
-    
+
     private void Start()
     {
         SpriteRenderer tileSr = null;
-       var pc = gameObject.GetComponentInChildren<PlatformCreator>();
-        if (pc != null&&pc.tiles.Length>0)
+        var pc = gameObject.GetComponentInChildren<PlatformCreator>();
+        if (pc != null && pc.tiles.Length > 0)
         {
             tileSr = pc.tiles[0].GetComponent<SpriteRenderer>();
         }
-        
-        if(tileSr!=null)
-        {
-            var height = tileSr.bounds.max.y - tileSr.bounds.min.y;
-            var zoneHeight = height * noFishZone;
 
-            topWall.transform.position = new Vector3(topWall.transform.position.x,
-                tileSr.gameObject.transform.position.y + height * 0.5f - zoneHeight,
-                topWall.transform.position.z
-                );
-            botWall.transform.position = new Vector3(botWall.transform.position.x,
-                tileSr.gameObject.transform.position.y - height * 0.5f + zoneHeight,
-                botWall.transform.position.z
-                );
+        if (tileSr != null)
+        {
+            var backgroundHeight = tileSr.bounds.max.y - tileSr.bounds.min.y;
+            var noFishZoneHeight = backgroundHeight * noFishZoneCoeff;
+            topBoundary = (backgroundHeight - noFishZoneHeight) * 0.5f;
+            bottomBoundary = -topBoundary;
+
         }
 
-        
+
         offset = cam.transform.position - fish.transform.position;
         frb = fish.GetComponent<Rigidbody2D>();
 
@@ -52,27 +45,18 @@ public class CameraController : MonoBehaviour
         if (GameController.instance.IsGameOver)
             return;
 
-        
-        
         cam.transform.position = fish.transform.position + offset;
-        
 
-        //       cam.transform.localPosition = new Vector3
-        //(
-        //   cam.transform.localPosition.x,
-        //    Mathf.Clamp(cam.transform.position.y, minimumBoundary, maximumBoundary),
-        //    cam.transform.position.z
-        //);
-        if (fish.transform.position.y + 2 > topWall.transform.position.y)
+
+        if (fish.transform.position.y >= topBoundary)
         {
             steerForce = Vector2.down;
-
         }
-        else if (fish.transform.position.y - 2 < botWall.transform.position.y)
+        else if (fish.transform.position.y <= bottomBoundary)
         {
             steerForce = Vector2.up;
         }
         else steerForce = Vector2.zero;
-        frb.AddForce(steerForce * fish.GetComponent<Move>().speed*Time.deltaTime, ForceMode2D.Impulse);
+        frb.AddForce(steerForce * fish.GetComponent<Move>().speed * Time.deltaTime, ForceMode2D.Impulse);
     }
 }
