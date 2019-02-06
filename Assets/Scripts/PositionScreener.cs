c9e4b8cf03eeb7c57e3d4a71d1c97f15be4e5518
Gleb <kosia4enko.g@ex.ua>
2019-02-06 10:00:51 +0200

    Fish bubbles
    Fish bubbles are going strictly upwards now. Created new script, Position screener. It has functionality same to old camera controller.

---
+++ Assets/Scripts/PositionScreener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionScreener : MonoBehaviour//screens translation of target to gameObject
+{
+    public Transform target;
+
+    private Vector3 difference;
+    void Start()
+    {
+        if (target != null)
+            difference = gameObject.transform.position - target.position;
+        else
+            Debug.LogError("target reference is not set to an instance of any object!");
+    }
+
+    public void LateUpdate()
+    {
+        if (target != null)
+            gameObject.transform.position = target.position + difference;
+    }
+
+}
